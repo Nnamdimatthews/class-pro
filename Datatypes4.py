b02d91d1d3346cee337926996fb2b90cdf135909
Nnamdimatthews <adaezeekoh.jc@gmail.com>
2026-04-28 11:43:36 +0100

    Add files via upload

---
+++ Datatypes4.py
@@ -0,0 +1,16 @@
+# Datatypes in Python
+# String, Integer, Float, Boolean
+
+Name = "Nicole"
+Age = 9
+Weight = 56.4
+Student = True
+
+print(f"Name : {Name}")
+print(f"Data Type of Name is {type(Name)}")
+print(f"Age : {Age}")
+print(f"Data Type of Age is {type(Age)}")
+print(f"Student : {Student}")
+print(f"Data Type of Student is {type(Student)}")
+print(f"Weight : {Weight}")
+print(f"Data Type of Weight is {type(Weight)}")

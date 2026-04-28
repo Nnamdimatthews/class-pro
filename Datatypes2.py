7883b4f101598ca248b5e720dbc415ce958c2113
Nnamdimatthews <adaezeekoh.jc@gmail.com>
2026-04-28 11:22:19 +0100

    Add files via upload

---
+++ Datatypes2.py
@@ -0,0 +1,15 @@
+# Coding: Typing datatypes in Python
+
+# 👇🏾String
+Name = "Nnamdi"
+# 👇🏾Integer
+Age = 8
+# 👇🏾Float
+Weight = 45.12
+# 👇🏾Boolean
+Student = True
+
+print(Name)
+print(Age)
+print(Weight)
+print(Student)

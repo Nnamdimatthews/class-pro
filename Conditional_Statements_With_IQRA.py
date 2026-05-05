b66f0db9f0a033072272b25269a870216782eef5
Nnamdimatthews <adaezeekoh.jc@gmail.com>
2026-05-05 11:27:35 +0100

    Add files via upload

---
+++ Conditional_Statements_With_IQRA.py
@@ -0,0 +1,19 @@
+num = 3
+if num > 0:
+    print(num, "Is a positive number")
+
+num2 = 0
+if num2 < 12:
+    print(num2, "Is a negative number")
+
+num3 = -1
+if num3 > 0:
+    print(num3, "Is a positive number")
+
+num4 = 0
+if num4 < 5:
+    print(num4, "Is a negative number")
+
+num5 = 19
+if num5 > 0:
+    print(num5, "Is a positive number")

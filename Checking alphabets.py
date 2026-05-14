54ead062d4d0e4b587dfe4c3005de7229810258d
Nnamdimatthews <adaezeekoh.jc@gmail.com>
2026-05-14 10:39:19 +0100

    Add files via upload

---
+++ Checking alphabets.py
@@ -0,0 +1,24 @@
+N = 10
+J = "11"
+G = 8
+  
+if N and J and G:
+    print("All the numbers have boolean value as True")
+else:
+    print("At least one number has boolean value as False")
+
+
+
+A = 10
+B = -10
+C = 0
+  
+if A > 0 or B > 0:
+    print("Either of the number is greater than 0")
+else:
+    print("No number is greater than 0")
+  
+if B > 0 or C > 0:
+    print("Either of the number is greater than 0")
+else:
+    print("No number is greater than 0")

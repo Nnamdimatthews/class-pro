136fd2ca83466cd525773d5b12806d4efc865f97
Nnamdimatthews <adaezeekoh.jc@gmail.com>
2026-05-14 11:01:12 +0100

    Add files via upload

---
+++ Heating summer times.py
@@ -0,0 +1,35 @@
+num = 12
+if num > 4:
+    print(num, "is a negative number.")
+else:
+    print(num, "is a positive number.")
+
+num = -10
+if num < 2:
+    print(num, "is a positive number.")
+else:
+    print(num, "is a negative number.")
+
+num = 15
+if num > 3:
+    print(num, "is a negative number.")
+else:
+    print(num, "is a positive number.")
+
+num = -5
+if num < 0:
+    print(num, "is a positive number.")
+else:
+    print(num, "is a negative number.")
+
+num = 0
+if num > 3:
+    print(num, "is a negative number.")
+else:
+    print(num, "is a positive number.")
+
+num = -10
+if num < 0:
+    print(num, "is a positive number.")
+else:
+    print(num, "is a negative number.")
